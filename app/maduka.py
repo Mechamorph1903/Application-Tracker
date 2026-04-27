46bf6111fcd65ddbb72a9e347eb7fe138cf886f4
Mechamorph1903 <danielanorue@gmail.com>
2026-04-27 11:11:29 -0500

    Learnt Render doesn't have chrome installed that's why slenium couldn't run, updated render.yaml to install &|| update chrome during build and added the path selenium should look for chrome in

--- app/maduka.py
+++ app/maduka.py
@@ -11,12 +11,6 @@ load_dotenv()
 api_key = os.getenv("CLAUDE_KEY")
 
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox") 
-options.add_argument("--disable-dev-shm-usage") 
-options.add_argument("--disable-gpu")
-options.add_argument("--remote-debugging-port=9222")
 
 def scrapeJob(url):
 	sehski = { # JSON data of 
@@ -24,7 +18,19 @@ def scrapeJob(url):
 		"Data": ""
 		} 
 		
+
+	driver = None
+
 	try:
+		options = webdriver.ChromeOptions()
+		options.add_argument("--headless")
+		options.add_argument("--no-sandbox") 
+		options.add_argument("--disable-dev-shm-usage") 
+		options.add_argument("--disable-gpu")
+		options.add_argument("--remote-debugging-port=9222")
+
+		if os.path.exists("/usr/bin/chromium"):
+			options.binary_location = "/usr/bin/chromium"
 	
 		driver = webdriver.Chrome(
 		service=Service(ChromeDriverManager().install()),
